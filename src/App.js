e8e56eb59b50985e1e1ef7d9783916253d7dc737
Brian Cheruiyot <briancheru11@gmail.com>
2025-08-01 15:02:29 +0300

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+
 function App() {
+  const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={pepperoniIsChecked}
+        onChange={e => setPepperoniIsChecked(e.target.checked)}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniIsChecked && <li>Pepperoni</li>}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
